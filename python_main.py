00717db41f7190714687e0f837b9ada0c9a90986
stephen <plarailsg@gmail.com>
2025-07-14 13:56:11 +0800

    Partially refactored code, Started Development of Predict/Restoring. New user_interface.py for UI-related handling.

--- python_main.py
+++ python_main.py
@@ -6,44 +6,21 @@ group_members = [
 
 from trie_editor import TrieEditor, Trie
 from feature_wildcard_search import WildcardSearchFeature
-
-# Function to display the banner
-def print_banner():
-    print("*****************************************************************")
-    print("* ST1507 DSAA: Predictive Text Editor (using tries)             *")
-    print("*---------------------------------------------------------------*")
-    print("*                                                               *")
-    print("* - Done by: Stephen Bermudo (2442657) & Aaron Ng (2442631)     *")
-    print("* - Class DAAA/2B/10                                            *")
-    print("*                                                               *")
-    print("*****************************************************************\n\n")
-
-# Function to display the menu
-def display_menu():
-    print("Please select your choice ('1','2','3','4','5','6','7'):")
-    print("    1. Construct/Edit Trie")
-    print("    2. Predict/Restore Text")
-    print("    ----------------------------------------------------")
-    print("    3. Extra Feature One (Stephen Bermudo):")
-    print("    4. Extra Feature Two (Stephen Bermudo):")
-    print("    ----------------------------------------------------")
-    print("    5. Extra Feature One (Aaron Ng):")
-    print("    6. Extra Feature Two (Aaron Ng):")
-    print("    ----------------------------------------------------")
-    print("    7. Exit")
+from user_interface import UserInterface
     
 # Main program loop
 def main():
-    print_banner()  # Show banner only once
+    UI = UserInterface()
     trie_editor = TrieEditor()
+    UI.print_banner()  # Show banner only once
 
     while True:
-        display_menu()
+        UI.display_menu()
         choice = input("Enter choice: ").strip()
         
         if choice == '1':
             print("You selected Option 1: Construct/Edit Trie\n")
-            trie_editor.command_prompt()
+            trie_editor.command_prompt("construct_edit")
         elif choice == '2':
             print("You selected Option 2: Predict/Restore Text\n")
             # Call your function here
